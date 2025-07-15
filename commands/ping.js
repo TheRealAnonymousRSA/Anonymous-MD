0f4633053d5887c4034e01c4f4a97bd1dd3a6015
TheRealAnonymousRSA <siphosethu69@outlook.com>
2025-07-15 05:47:47 +0200

    Create commands/ping.js

---
+++ commands/ping.js
@@ -0,0 +1,8 @@
+js
+module.exports = {
+  name: "ping",
+  description: "Check bot response",
+  execute: async (sock, m) => {
+    await sock.sendMessage(m.key.remoteJid, { text: "🏓 Pong!" }, { quoted: m });
+  }
+};
