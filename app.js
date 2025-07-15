efa247079fef9e0d26157eb8007282440ff542d3
TheRealAnonymousRSA <siphosethu69@outlook.com>
2025-07-15 11:29:34 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+js
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("🤖 Anonymous-MD Bot is live.");
+});
+
+app.listen(PORT, () => {
+  console.log(`🌐 App running on http://localhost:${PORT}`);
+});
