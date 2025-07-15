9359a17a1e8fa00b8b8c4b37b8774c64ccfff8ed
TheRealAnonymousRSA <siphosethu69@outlook.com>
2025-07-15 05:52:18 +0200

    Create menu.js

---
+++ commands/menu.js
@@ -0,0 +1,16 @@
+js
+module.exports = {
+  name: "menu",
+  description: "Displays bot menu",
+  execute: async (sock, m) => {
+    const menu = `🤖 *Anonymous-MD Menu*
+    
+Available Commands:
+/ping - Check bot status
+/menu - Show this menu
+
+More features coming soon...`;
+
+    await sock.sendMessage(m.key.remoteJid, { text: menu }, { quoted: m });
+  },
+};
