0bc8c2d0db7d3525970a0ec7d7505fa5de5973f4
TheRealAnonymousRSA <siphosethu69@outlook.com>
2025-07-15 05:59:00 +0200

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,17 @@
+js
+module.exports = {
+  name: "help",
+  description: "Display help information",
+  async execute(sock, m) {
+    const helpMessage = `
+🤖 *Anonymous-MD Bot Help Menu*
+
+- /ping : Check if the bot is alive
+- /menu : Show main menu
+- /help : Show this help message
+
+More commands coming soon!
+    `;
+    await sock.sendMessage(m.key.remoteJid, { text: helpMessage }, { quoted: m });
+  },
+};
